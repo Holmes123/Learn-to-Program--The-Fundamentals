23b5e23e7e31feb2db64cde64b6eea9cf8a9981a
Claus Holm Larsen <chl@1-holmes.dk>
2013-09-04 12:51:52 +0200

    Python 3.3 Fundamental Programming - Lesson 1
    How to print to the console and other useful print statements.

---
+++ Lesson_1.py
@@ -0,0 +1,50 @@
+####################################################
+####################################################
+############# Welcome to lesson 1 ##################
+##############    Python 3.3       #################
+####################################################
+####################################################
+
+# You may at this point have noticed loads of hash signs - # - and are wondering why?
+# Well, the hash sign # is used  as a starter sign for a line where you'll place a comment to your code.
+# What ever comes after the # is not read as a code, but merely as what it is:
+# A note, message or explanation to the code.
+
+print ("Be carefull not to write too long lines. Try to keep the length of your lines below 80 carracters.")
+# This is a comment. Text in comments have a different color as do statements like print.
+print ("Comments can be added at the end of a line to explain the code.")
+print ()
+print ('kelvin = celcius * 273.14    # Inserts the degrees in celcius.')
+print ()
+print ("You can also print multiple lines with only one print statement.")
+print ('To do this you must enclose the text with 3 """double quotes at the beginning and 3 """double quotes at the end of the text.')
+print ("""This text is printed on several lines.
+Here we used 3 double quotes in the beginning and end of
+the text. Note that the line shift comes automaticly.""")
+print ('''Naturally you can also use the single quotes
+to print long text to the console window.
+And single quotes obey the same rules as double quotes.
+By using single quotes you can include the double quotes in the text.
+"He said hello to the world."''')
+print ()
+print ("""Often you have to make longer comments to your code.
+To avoid filling your code with the hash sign you simply enclose
+the comment with 3 '''single quotes at the beginning and end of the comment.'''
+This allows you to write multiple lines of comments to you code""")
+print ()
+'''
+Here I've made my own comment to my code. I preferr to start the line comment with the 3
+single quotes and then make a line shift like I've done here. I also place the last 3 single quotes 
+on a separate line. This makes it much easier to spot longer comments.
+Remember, that you can't make enough comments to your code. In the long term it ease
+up the understanding of your code, especially when you after a while returns
+to review the code.'''
+
+"""Naturally you can use # " at the beginning and 3 " at the end of 
+longer comments.
+"""
+print ()
+print ("I'll in a later file introduce you to more advances use of the 'print' statement.")
+print ()
+print ("So off to the next lesson. Se lesson_2.py.")
+
